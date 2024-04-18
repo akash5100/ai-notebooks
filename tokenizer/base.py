a89475db959d76f64a3275de79a90f291a87a03e
akash5100 <shivamvermashs@gmail.com>
2024-04-18 23:14:29 +0530

    base bytepair tokenizer

---
+++ tokenizer/base.py
@@ -0,0 +1,141 @@
+# karpthy/minbpe ripoff 
+
+# Helper function =================================
+
+def get_stats(tokens):
+  toks = {}
+  for pair in zip(tokens, tokens[1:]):
+    toks[pair] = toks.get(pair, 0) + 1
+  return toks
+
+def merge(tokens, pair, idx):
+  i = 0
+  new_tokens = []
+  while i < len(tokens):
+    if i < len(tokens)-1 and (tokens[i], tokens[i+1]) == pair:
+      new_tokens.append(idx)
+      i+=2
+    else:
+      new_tokens.append(tokens[i])
+      i+=1
+  return new_tokens
+
+# =============== Regex helpers ====================
+
+def replace_control_characters(s: str) -> str:
+  # we don't want to print control characters
+  # which distort the output (e.g. \n or much worse)
+  # https://stackoverflow.com/questions/4324790/removing-control-characters-from-a-string-in-python/19016117#19016117
+  # http://www.unicode.org/reports/tr44/#GC_Values_Table
+  import unicodedata
+  chars = []
+  for ch in s:
+    if unicodedata.category(ch)[0] != "C":
+      chars.append(ch) # this character is ok
+    else:
+      chars.append(f"\\u{ord(ch):04x}") # escape
+  return "".join(chars)
+
+def render_token(t: bytes) -> str:
+    # pretty print a token, escaping control characters
+    s = t.decode('utf-8', errors='replace')
+    s = replace_control_characters(s)
+    return s
+
+# ===================================================
+
+# base tokenizer class
+class BaseTokenizer:
+  def __init__(self) -> None:
+    self.merges = {}                 # (int, int) -> int
+    self.pattern = ""                # str
+    self.special_tokens = {}         # str -> int, e.g. {'<|endoftext|>': 100257}
+    self.vocab = self._build_vocab() # int -> bytes
+
+
+  def _build_vocab(self):
+    # vocab is simply, with rules, derived from merges
+    vocab = {i:bytes([i]) for i in range(256)}
+    for (p0, p1), v in self.merges.items():
+      vocab[v] = vocab[p0] + vocab[p1]
+    for tok, v in self.special_tokens.items():
+      vocab[v] = tok.encode('utf-8')
+    return vocab
+
+  def train(self, text, vocab_size=256, verbose=False):
+    raise NotImplementedError
+  
+  def encode(self, text):
+    raise NotImplementedError
+
+  def decode(self, ids):
+    raise NotImplementedError
+
+  def save(self, file_name):
+    """
+    Saves two files: file.vocab and file.model
+    This is inspired (but not equivalent to!) sentencepiece's model saving:
+    - model file is the critical one, intended for load()
+    - vocab file is just a pretty printed version for human inspection only
+    """
+    model_file = file_name + ".model"
+    with open(model_file, 'w') as f:
+      # write the version, pattern and merges, that's all that's needed
+      f.write("minbpe v1\n")
+      # pattern used
+      f.write(f"{self.pattern}\n")
+      # numbers of special tokens
+      f.write(f"{len(self.special_tokens)}\n")
+      # write special tokens
+      for special, idx in self.special_tokens.items(): 
+        f.write(f"{special} {idx}\n")
+      # write merges
+      for idx1, idx2 in self.merges:
+        f.write(f"{idx1} {idx2}\n")
+
+    vocab_file = file_name + ".vocab"
+    inverted_merges = {idx: pair for pair, idx in self.merges.items()}
+    with open(vocab_file, "w", encoding="utf-8") as f:
+      for idx, token in self.vocab.items():
+        # note: many tokens may be partial utf-8 sequences
+        # and cannot be decoded into valid strings. Here we're using
+        # errors='replace' to replace them with the replacement char �.
+        # this also means that we couldn't possibly use .vocab in load()
+        # because decoding in this way is a lossy operation!
+        s = render_token(token)
+        # find the children of this token, if any
+        if idx in inverted_merges:
+          # if this token has children, render it nicely as a merge
+          idx0, idx1 = inverted_merges[idx]
+          s0 = render_token(self.vocab[idx0])
+          s1 = render_token(self.vocab[idx1])
+          f.write(f"[{s0}] [{s1}] -> [{s}] {idx}\n")
+        else:
+          # (this should just be the first 256 tokens, the bytes)
+          f.write(f"[{s}] {idx}\n")
+
+
+  def load(self, file):
+    """Loads the file.model"""
+    assert file.endswith(".model")
+    merges = {}
+    special_tokens = {}
+    idx = 256
+    with open(file, 'r', encoding="utf-8") as f:
+      # confirm metadata
+      version = f.readline().strip()
+      assert version == "minbpe v1"
+      self.pattern = f.readline().strip()
+      # special tokens
+      num_special = int(f.readline().strip())
+      for _ in range(num_special):
+        special, special_idx = f.readline().strip().split()
+        special_tokens[special] = int(special_idx)
+      # read the merges
+      for line in f:
+        idx1, idx2 = map(int, line.split())
+        merges[(idx1, idx2)] = idx
+        idx += 1
+    self.merges = merges
+    self.special_tokens = special_tokens
+    self.vocab = self._build_vocab()
---
+++ tokenizer/basic_bpe.py
@@ -0,0 +1,58 @@
+from base import BaseTokenizer, merge, get_stats 
+
+
+class BasicTokenizer(BaseTokenizer):
+  def __init__(self):
+    super().__init__()
+
+  def train(self, text, vocab_size=256, verbose=False):
+    """builds merges"""
+    num_merges = vocab_size - 256
+    merges = {}
+    vocab = {i:bytes([i]) for i in range(256)}
+    tokens = list(text.encode('utf-8'))
+    for i in range(num_merges):
+      stats = get_stats(tokens)
+      pair = max(stats, key=stats.get) # maximum occured pair
+      idx = 256+i
+      tokens = merge(tokens, pair, idx)
+      merges[pair] = idx
+      vocab[idx] = vocab[pair[0]] + vocab[pair[1]] # instead of _build, create while running
+      if verbose:
+        print(f"[{pair[0]:5d}, {pair[1]:5d}] -> {256+i}")
+
+    self.merges = merges
+    self.vocab = vocab
+
+
+  def encode(self, text: str) -> list:
+    tokens = text.encode('utf-8')
+    while len(tokens) > 1:
+      stats = get_stats(tokens)
+      pair = min(stats, key=lambda p: self.merges.get(p, float('inf')))
+      if pair not in self.merges:
+        break
+      idx = self.merges[pair]
+      tokens = merge(tokens, pair, idx) 
+    return tokens
+
+
+  def decode(self, tokens: list) -> str:
+    bin_text = b"".join(self.vocab[i] for i in tokens)
+    return bin_text.decode('utf-8')
+
+
+if __name__ == '__main__':
+
+  tcn = BasicTokenizer()
+  # loading data to train tokenizer
+  # with open('../data/cs.txt', 'r') as f:
+  #   text = f.read()
+  # tcn.train(text, 256 + 3000, verbose=True) # 256 are the byte tokens, then do 3k merges
+  # tcn.save('bpe3k')
+
+  tcn.load('bpe3k.model')
+  tcn.encode('Computer Science, technology')
+  # [1634, 962, 559, 121]
+  tcn.decode([1634, 962, 559, 121])
+  # 'Computer Science, technology'
